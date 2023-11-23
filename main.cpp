7d2eabb27243be17aa340343344b172c25ca3f51
Quatreble <quatreble@gmail.com>
2023-11-23 13:30:38 +0200

    First real commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+class Person{
+public:
+    string first_name;
+    string last_name;
+    int age;
+    string id_code;
+public:
+    Person(string f_name, string l_name)
+    : first_name(f_name), last_name(l_name)
+    {}
+
+    ~Person(){
+        cout << "Destructed!";
+    }
+
+
+};
+
 int main(){
-    cout<<"elpidaaaaaaaa"<<endl;
+    Person elpida("elpida", "stergiou");
+    cout << elpida.first_name << " " << elpida.last_name << endl;
 }
